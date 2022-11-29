d781b77847064a6d60a4cad1b9e0634ff3cd7739
Deby2021 <dcc.cabrera1986@gmail.com>
2022-11-29 18:24:17 -0300

    Inserindo animação - LifeStatus e estrututando compomentes

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, Image, ScrollView, StyleSheet } from "react-native";
+
+import LifeStatus from "../../Components/Common/LifeStatus";
 
 export default function Start() {
   return (
     <View style={styles.container}>
-      <ScrollView showsVerticalScrollIndicator={false}>
+      <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
         <View style={{ alignItems: "center" }}>
           <Image
             source={require("../../assets/icons/logo3.png")}
             style={styles.logo}
           />
+          <LifeStatus />
           <Text style={styles.description}>
             Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"}{" "}
             o melhor nível.
