7a5d87cdc3f0d72034fa5b43800ed09834eafb62
Deby2021 <dcc.cabrera1986@gmail.com>
2022-12-02 12:48:33 -0300

    Renderização condicional de página e tempo de vida dinaminco

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,8 +1,15 @@
 import { useState } from "react";
+import ChangeNavigationService from "../Services/ChangeNavigationService";
 import AllPages from "./AllPages";
 
 export default function Routes() {
   const [showHome, setShowHome] = useState("false");
+
+  ChangeNavigationService.checkShowHome(1)
+    .then((showHome) => {
+      setShowHome(showHome.showHome);
+    })
+    .catch((err) => console.log(err));
 
   //recebo a condição completa
   //se o usuario ja
